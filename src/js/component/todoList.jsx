2c71b6344d912643133db6860f84354de0e7be70
oscardanielmejiar <oscardanielmejiar@gmail.com>
2022-10-06 23:15:28 +0000

    getting

---
+++ src/js/component/todoList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TodoForm = () =>{
+    return(<div>
+        <ul className="list-group">
+        <li className="list-group-item">Hola</li>
+        <li className="input-group">
+  <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+  <button className="btn btn-outline-secondary" type="button" id="button-addon2">Button</button></li>
+</ul>
+    </div>)
+}
+
+export default TodoForm
