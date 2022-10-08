d69d38ac9390ed6d1424d18f487886deaf91a404
oscardanielmejiar <oscardanielmejiar@gmail.com>
2022-10-08 00:49:03 +0000

    on going

--- src/js/component/todoList.jsx
+++ src/js/component/todoList.jsx
@@ -10,7 +10,7 @@ const TodoForm = () =>{
   <input type="text"  onChange={e => setInputValue(e.target.value)} value={inputValue}
   className="form-control" placeholder="No tasks, add a task" aria-label="No tasks, add a task" aria-describedby="button-addon2"/>
   <button className="btn btn-secondary" type="button" id="inputGroupFile01">Add Task</button></li>
-  <li className="list-group-item">Hola</li>
+  <li className="list-group-item">{inputValue}</li>
   <li className="list-group-item">Hola</li>
   <li className="list-group-item">Hola</li>
   <li className="list-group-item">Hola</li>
